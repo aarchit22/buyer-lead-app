28ea7179ace71bb6f6ef735e228ef3346aa79285
aarchit22 <architatrey04a@gmail.com>
2025-09-12 18:34:04 +0530

    feat: added config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // This is the crucial part for path aliases
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
